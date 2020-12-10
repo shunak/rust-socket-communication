0439218b2716e3cbbfd9070294a04c89d59d75d8
shunak <shuna966@gmail.com>
2020-12-10 17:09:00 +0900

    implement main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,62 @@
+use std::env;
+#[macro_use]
+extern crate log;
+
+mod tcp_client;
+mod tcp_server;
+mod udp_client;
+mod udp_server;
+
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG","debug");
+    env_logger::init();
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() !=4 {
+        error!("Please specify [tcp | udp] [server|client] [addr:port]".);
+        std::process::exit(1);
+    }
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    let address: &str = &args[3];
+
+    match protocol{
+        "tcp"=> match role {
+            "server" => {
+                // call tcp server
+            }
+            "client" =>{
+                // call tcp client
+            }
+            _ => {
+                missing_role();
+            }
+        },
+        "udp" => match role {
+           "server" => {
+               // call udp server
+           }
+           "client" => {
+               //call udp client
+           }
+           _ => {
+               missing_role();
+           }
+        },
+        _ => {
+            error!("Please specify tcp or udp on the 1st argument.");
+            std::process::exit(1);
+        }
+    }
 }
 
+
+fn missing_role(){
+    error!("Please specify server or client on the 2nd argument.");
+    std::process::exit(1);
+}
 
 
 
