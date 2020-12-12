c725fcd4b78d27e422bd26f18774911d9b4055ca
shunak <shuna966@gmail.com>
2020-12-13 00:43:22 +0900

    added: send input data from socket

--- src/tcp_client.rs
+++ src/tcp_client.rs
@@ -2,6 +2,16 @@ use std::io{self,BufRead,BufReader,Write};
 use std::net::TcpStream;
 use std::str;
 
-/* rake connection directed IP address and Port */
+use failure::Fail;
 
+/* rake connection directed IP address and Port */
+pub fn connect (address: &str) -> Result<(), failure::Error>{
+    let mut stream = TcpStream::connect(address)?;
+    loop{
+        /* send input data from socket*/
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        stream.write_all(input.as_bytes())?;
+    }
+}
 
