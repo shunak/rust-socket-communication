6b7cc9c8424a58f749316944611ce57b9e8ef6d5
shunak <shuna966@gmail.com>
2020-12-14 00:01:27 +0900

    modified

--- src/tcp_client.rs
+++ src/tcp_client.rs
@@ -10,13 +10,12 @@ pub fn connect (address: &str) -> Result<(), failure::Error>{
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
         stream.write_all(input.as_bytes())?;
+        /* display received data from socket */
+        let mut reader = BufReader::new(&stream);
+        let mut buffer = Vec::new();
+        reader.read_until(b'\n', &mut buffer)?;
+        print!("{}",str::from_utf8(&buffer)?);
     }
-    /* display received data from socket */
-    let mut reader = BufReader::new(&stream);
-    let mut buffer = Vec::new();
-    reader.read_until(b'\n', &mut buffer)?;
-    print!("{}",str::from_utf8(&buffer)?);
- 
 }
 
 
