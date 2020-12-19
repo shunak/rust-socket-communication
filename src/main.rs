2a7bec2ba1676495e3d2bcc55c348ba3c79a4136
shunak <shuna966@gmail.com>
2020-12-20 00:44:57 +0900

    added

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ fn main() {
             }
             "client" =>{
                 // call tcp client
+                tcp_client::connect(address).unwrap_or_else(|e| error!("{}",e));
             }
             _ => {
                 missing_role();
